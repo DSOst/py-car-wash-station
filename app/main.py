91ebd53d0dd5070b6a27a40605c07cd058aba853
Dmytro Ostapenko <dr.dm.ostapenko@gmail.com>
2025-11-14 13:31:40 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -8,27 +8,25 @@ class Car:
 
 class CarWashStation:
     def __init__(self, distance_from_city_center: float, clean_power: int,
-                 average_rating: float, count_of_rating: int) -> None:
+                 average_rating: float, count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
-        self.count_of_ratings = count_of_rating
+        self.count_of_ratings = count_of_ratings
 
     def calculate_washing_price(self, car: Car) -> float:
         if self.clean_power <= car.clean_mark:
             return 0.0
-        else:
-            price = (car.comfort_class * (self.clean_power - car.clean_mark)
-                     * self.average_rating / self.distance_from_city_center)
-            return round(price, 1)
+        price = (car.comfort_class * (self.clean_power - car.clean_mark)
+                 * self.average_rating / self.distance_from_city_center)
+        return round(price, 1)
 
     def wash_single_car(self, car: Car) -> float:
-        if self.clean_power > car.clean_mark:
-            price = self.calculate_washing_price(car)
-            car.clean_mark = self.clean_power
-            return price
-        else:
+        if self.clean_power <= car.clean_mark:
             return 0.0
+        price = self.calculate_washing_price(car)
+        car.clean_mark = self.clean_power
+        return price
 
     def serve_cars(self, cars: list[Car]) -> float:
         income = 0.0
